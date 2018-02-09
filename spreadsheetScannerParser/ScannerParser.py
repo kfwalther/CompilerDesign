e86e45e19d7052b23c07050d01a78db1856fea5e
Kevin Walther <kfwalther@gmail.com>
2018-02-09 00:50:57 -0500

    Adding initial Spreadsheet Scanner Parser code
    Added some boilerplate code for reading/writing to files

--- spreadsheetScannerParser/ScannerParser.py
+++ spreadsheetScannerParser/ScannerParser.py
@@ -7,7 +7,31 @@
 
 '''
 
+import os
+import sys
 
+
+
+# Process the input file line by line.
+def processInputFile(inputFilePath):
+  with open(inputFilePath, 'r') as fileId:
+		for line in fileId:
+      if line[0] == '#':
+        print('Encountered comment')
+      else:
+				#scanner.processInputLine(line)
+
+# Run the Scanner Parser.
+def runScannerParser(inputFile, outputFile):
+	inputFilePath) = os.path.join(os.path.dirname(os.path.abspath(__file__)), inputFile)
+	outputFilePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), outputFile)
+  processInputFile(inputFilePath)
+	
+		
+# Begin code execution here.
+if __name__ == "__main__":
+	runScannerParser('HW2-Input.txt', 'output.txt')
+	
 
 
 
