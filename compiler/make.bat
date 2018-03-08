eddc0430b620eb92ad595dd0593567472d9343fb
walther.laptop <kfwalther@gmail.com>
2018-03-08 01:18:22 -0500

    Compiler: 4 hours later...
    Can now compile the Flex/Bison combo into C++, instead of C. Flex
    created a yyFlexLexer implementation of yylex, so Bison's yyparse
    couldn't see it. Had to redirect the yylex calls to the Flex-generated
    version. Incredible how clunky and messy the Flex/Bison code is...

--- compiler/make.bat
+++ compiler/make.bat
@@ -1,18 +1,22 @@
 :: Batch script for creating the parser using Bison.
 
-:: Call Flex to auto-generate the C code for the scanner.
-win_flex --outfile=flexGeneratedScanner.c flexScannerSource.l
-
 :: Call Bison to auto-generate the C code.
 :: -d directive will produce a header file as well.
-win_bison -d bisonParserSource.y ^
---output=bisonGeneratedParser.c ^
+win_bison -d bisonParserSource.ypp ^
+--output=bisonGeneratedParser.cpp ^
 --verbose ^
 --graph=parseGraph.dot
 
+:: Call Flex to auto-generate the C code for the scanner.
+win_flex --c++ --outfile=flexGeneratedScanner.cpp flexScannerSource.l
+::win_flex flexScannerSource.l
+
 :: Call GCC to compile the C code into an executable.
 :: Put gcc option '-w' to hide all warnings.
-gcc -o flexBisonScannerParser.exe bisonGeneratedParser.c flexGeneratedScanner.c -w
+:: Put gcc option '-Wall -g' to get all warnings.
+g++ -c bisonGeneratedParser.cpp -o bisonGeneratedParser.o -Wall -isystem "C:\Users\walther\win_flex_bison-2.5.1" 
+g++ -c flexGeneratedScanner.cpp -o flexGeneratedScanner.o -Wall -isystem "C:\Users\walther\win_flex_bison-2.5.1" 
+g++ flexGeneratedScanner.o bisonGeneratedParser.o -o flexBisonScannerParser.exe -w 
 
 :: Run dot.exe on the generated Bison digraph to generate visualization.
-dot -T pdf parseGraph.dot -o parseGraph.pdf
+::dot -T pdf parseGraph.dot -o parseGraph.pdf
